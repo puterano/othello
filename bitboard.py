6afe1981429c5247fd121c3ea2d2698d3297702d
puterano <putera0120@gmail.com>
2024-02-08 15:46:28 -0800

    small changes modify printboard (allow printing for negative binary) modify masks (invert at beginning) modify stones counting(50-90% decrease in time)

--- bitboard.py
+++ bitboard.py
@@ -1,37 +1,58 @@
-NOLEFT = 0x820820820
-NOUP = 0xfc0000000
-NODOWN = 0x00000003f
-NORIGHT = 0x041041041
+NOLEFT = ~0x820820820
+NOUP = ~0xfc0000000
+NODOWN = ~0x00000003f
+NORIGHT = ~0x041041041
+
 from math import log
+import time
+total_time = 0
+timed = 0
 
 class Othelloboard():
 
     def __init__(self, white, black, playerwhite):
+        # global total_time
+        # global timed
+        # timed += 1
+
+        # start = time.time()
         self.white = white
         self.black = black
         self.placing = playerwhite
-    
+
+        # total_time += time.time() - start
+
+
     def switch(self):
         self.placing = -self.placing
 
     def printboard(self):
-        print('  ', end = '')
-        for index in range(6):
-            print(chr(ord('a') + index), end=' ')
-        print()
-        white = bin(self.white)[2:].zfill(36)
-        black = bin(self.black)[2:].zfill(36)
-        for digits in range(36):
-            if digits % 6 == 0:
-                print(digits // 6 + 1, end=' ')
-            if white[digits] == '1':
-                print('●', end = ' ')
-            elif black[digits] == '1':
-                print('○', end = ' ')
+        field = [['']*6 for _ in range(6)]
+        white, black = self.white, self.black
+        location = 0
+        for location in range(36):
+
+            if white & 1:
+                field[-(location // 6 + 1)][-(location % 6 + 1)] = '●'
+            elif black & 1:
+                field[-(location // 6 + 1)][-(location % 6 + 1)] = '○'
             else:
-                print('-', end = ' ')
-            if digits % 6 == 5:
-                print()
+                field[-(location // 6 + 1)][-(location % 6 + 1)] = '-'
+
+            white >>= 1
+            black >>= 1
+        
+        print(' ', end = ' ')
+        for index in range(6):
+            print(chr(ord('a') + index), end = ' ')
+        print()
+
+        for line_number, line in enumerate(field):
+            print(line_number + 1, end = ' ')
+            for cells in line:
+                print(cells, end = ' ')
+            print()
+
     
     def printwithhint(self, locations = None):
         if not locations:
@@ -84,14 +105,14 @@ class Othelloboard():
             opponentboard = self.white
         temp = board
         for _ in range(6):
-            temp &= ~directionboard
+            temp &= directionboard
             if leftorright == 1:
                 temp <<= number_to_shift
             else:
                 temp >>= number_to_shift
             temp &= opponentboard
             potentialenemy |= temp
-        potentialenemy &= ~directionboard
+        potentialenemy &= directionboard
         if leftorright == 1:
             potentialenemy <<= number_to_shift
         else:
@@ -99,11 +120,18 @@ class Othelloboard():
         return potentialenemy & ~(board | opponentboard)
     
     def abletoplace(self):
+        # global total_time
+        # global timed
+        # start = time.time()
+        # timed += 1
+
         abletoplace = self._search_for_place_location_in_direction(NOLEFT, 1, 1)
         for i in {(NOUP, 1, 6), (NODOWN, -1, 6), (NORIGHT, -1, 1), 
-                    (NOUP | NOLEFT, 1, 7), (NOUP | NORIGHT, 1, 5),
-                    (NODOWN | NOLEFT, -1, 5), (NODOWN | NORIGHT, -1, 7)}:
+                    (NOUP & NOLEFT, 1, 7), (NOUP & NORIGHT, 1, 5),
+                    (NODOWN & NOLEFT, -1, 5), (NODOWN & NORIGHT, -1, 7)}:
             abletoplace |= self._search_for_place_location_in_direction(*i)
+
+        # total_time += time.time() - start
         return abletoplace
     
     def _search_for_invert_position_in_direction(self, location_to_place, leftorright, length_of_shift, masking_board):
@@ -115,7 +143,7 @@ class Othelloboard():
             playerboard = self.black
             opponentboard = self.white
         for _ in range(6):
-            location_to_place &= ~masking_board
+            location_to_place &= masking_board
             if leftorright == 1:
                 location_to_place <<= length_of_shift
             else:
@@ -130,13 +158,18 @@ class Othelloboard():
         return potentialplace
     
     def invertboard(self, location):
+        # global total_time
+        # global timed
+        # start = time.time()
+        # timed += 1
+
         if self.placing == 1:
             self.white |= location
         else:
             self.black |= location
-        for inputs in [(1,1,NOLEFT), (1,6,NOUP), (1,7,NOLEFT|NOUP), 
-                        (1,5,NORIGHT|NOUP), (-1,1,NORIGHT), (-1,6,NODOWN), 
-                        (-1,7,NODOWN|NORIGHT), (-1,5,NODOWN|NOLEFT)]:
+        for inputs in [(1,1,NOLEFT), (1,6,NOUP), (1,7,NOLEFT & NOUP), 
+                        (1,5,NORIGHT & NOUP), (-1,1,NORIGHT), (-1,6,NODOWN), 
+                        (-1,7,NODOWN & NORIGHT), (-1,5,NODOWN & NOLEFT)]:
             fornow = self._search_for_invert_position_in_direction(location, *inputs)
             if self.placing == 1:
                 self.white |= fornow
@@ -144,22 +177,42 @@ class Othelloboard():
             else:
                 self.black |= fornow
                 self.white ^= fornow
+        
+        # total_time += time.time() -start
 
-    def get_winner(self):
-        white_count = 0
-        black_count = 0
-        while self.white or self.black:
 
-            if self.white & 1:
-                white_count += 1
-            if self.black & 1:
-                black_count += 1
+    def get_winner(self): # became little faster -- 0.09 -> 0.01 to 0.04
+        
+        global total_time
+        # global timed
+        start = time.time()
+        # timed += 1
+        
+        self.white = self.white - ((self.white >> 1) & 0x555555555)
+        self.black = self.black - ((self.black >> 1) & 0x555555555)
+        
+        self.white = (self.white & 0x333333333) + ((self.white >> 2) & 0x333333333)
+        self.black = (self.black & 0x333333333) + ((self.black >> 2) & 0x333333333)
+        
+        self.white = (self.white + (self.white >> 4)) & 0xf0f0f0f0f
+        self.black = (self.black + (self.black >> 4)) & 0xf0f0f0f0f
+        
+        self.white = self.white + (self.white >> 8)
+        self.black = self.black + (self.black >> 8)
+        
+        self.white = self.white + (self.white >> 16)
+        self.black = self.black + (self.black >> 16)
+        
+        self.white = self.white + (self.white >> 32)
+        self.black = self.black + (self.black >> 32)
+        
+        self.white, self.black = self.white & 63, self.black & 63
 
-            self.white >>= 1
-            self.black >>= 1
-            
-        if white_count > black_count:
+        total_time += time.time() - start
+
+        if self.white > self.black:
             return 1
-        elif black_count > white_count:
+        elif self.white < self.black:
             return -1
-        return 0
+        else:
+            return 0
